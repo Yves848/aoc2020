7c8f7290412c5d3542a5009bb963b02123e18295
yves848 <yves.godart@gmail.com>
2024-12-27 16:36:44 +0100

    WIP

--- 04/Program.cs
+++ 04/Program.cs
@@ -1,7 +1,18 @@
 ﻿using System.Text.RegularExpressions;
 
 var file = args.Length > 0 ? File.ReadAllText(args[0]) : File.ReadAllText(@"C:\Users\yvesg\git\aoc2020\04\test.txt");
-List<string> fields = {""};
+List<string> fields = new List<string> {"byr:","iyr:","eyr:","hgt:","hcl:","ecl:","pid:","cid:"};
+/*
+byr (Birth Year)
+iyr (Issue Year)
+eyr (Expiration Year)
+hgt (Height)
+hcl (Hair Color)
+ecl (Eye Color)
+pid (Passport ID)
+cid 
+*/
+
 
 void part1()
 {
@@ -9,14 +20,35 @@ void part1()
   string pattern = @"(\w+):([#|\w]+)";
   Regex re = new Regex(pattern);
   var passports = file.Split("\r\n\r\n");
-
+  passports.ToList().ForEach(p => {
+    MatchCollection check = re.Matches(p);
+    if (check.Count == fields.Count) {
+      ans++;
+    } else {
+      if (!p.Contains("cid:") && check.Count == fields.Count-1) ans++;
+    }
+  });
   Console.WriteLine($"Part 1 - Answer : {ans}");
 }
 
 void part2()
 {
   int ans = 0;
-  
+  string pattern = @"(\w+):([#|\w]+)";
+  Regex re = new Regex(pattern);
+  var passports = file.Split("\r\n\r\n");
+  passports.ToList().ForEach(p => {
+    p = p.Replace("\r\n"," ");
+    MatchCollection check = re.Matches(p);
+    bool valid = (check.Count == fields.Count) || (!p.Contains("cid:") && check.Count == fields.Count-1);
+    if (valid) {
+      int i = 0;
+      while (valid && i < check.Count) {
+
+        i++;
+      }
+    }
+  });
 
   Console.WriteLine($"Part 2 - Answer : {ans}");
 }
--- 04/obj/Debug/net9.0/04.AssemblyInfo.cs
+++ 04/obj/Debug/net9.0/04.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("04")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58f05419b88d2a2fa41f19808df6338171d97c7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64a8b1714a7cd33b57e2d006b484c337e7e250e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("04")]
 [assembly: System.Reflection.AssemblyTitleAttribute("04")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
