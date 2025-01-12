dda3cc679aecc29d519d2474519c19616dc7b281
Yves <yves@pop-os.ccircus>
2025-01-12 15:26:06 +0100

    18-2

--- 18/Program.cs
+++ 18/Program.cs
@@ -1,29 +1,109 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.ComponentModel;
+using System.Text.RegularExpressions;
 
 string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-List<string> file = args.Length > 0 ? File.ReadAllLines(args[0]).ToList() : File.ReadAllLines($"{home}\\git\\aoc2020\\18\\test.txt").ToList();
+List<string> file = args.Length > 0 ? File.ReadAllLines(args[0]).ToList() : File.ReadAllLines($"{home}/git/aoc2020/18/test.txt").ToList();
 
-string removeP(string l) {
-  int p = l.IndexOf('(');
-  if (p > 0) l = removeP(l.Substring(p+1));
-  
-  return "";
+long compute(string line, char[] rule)
+{
+  var l = Regex.Replace(line, @"\s+", " ").Trim().Split(' ').ToList();
+  long ans = 0;
+  rule.ToList().ForEach(r =>
+  {
+    int i = 0;
+    while (i < l.Count-1)
+    {
+      if (l[i] == r.ToString())
+      {
+        long op1 = long.Parse(l[i - 1]);
+        long op2 = long.Parse(l[i + 1]);
+        if (r == '+')
+        { 
+          l[i] =  (op1 + op2).ToString();
+          l.RemoveAt(i+1);
+          l.RemoveAt(i-1);
+          i = 0;
+        } else {
+          l[i] =  (op1 * op2).ToString();
+          l.RemoveAt(i+1);
+          l.RemoveAt(i-1);
+          i = 0;
+        }
+      }
+      i++;
+    }
+  });
+  ans = long.Parse(l[0]);
+  return ans;
 }
 
-long resolve(string l) {
-  int p = l.IndexOf('(');
-  while (p > -1) {
-    removeP(l.Substring(p+1));
-    p = l.IndexOf('(');
+
+long compute2(string line, char[] rule)
+{
+  var l = Regex.Replace(line, @"\s+", " ").Trim().Split(' ').ToList();
+  long ans = int.Parse(l[0]);
+  int i = 1;
+  while (i < l.Count - 1)
+  {
+    if (l[i] == "+")
+    {
+      ans += long.Parse(l[i + 1]);
+    }
+    else
+    {
+      ans *= long.Parse(l[i + 1]);
+    }
+    i += 2;
   }
-  return 0;
+  return ans;
+}
+
+long resolve(string l, char[] rule)
+{
+  Regex p = new Regex(@"[\(\|\)]");
+  Stack<int> Q = [];
+  MatchCollection m = p.Matches(l);
+  if (m.Count > 0)
+  {
+    int i = 0;
+    Q.Push(m[i].Index);
+    i++;
+    while (i < m.Count)
+    {
+      if (m[i].Value == "(")
+      {
+        Q.Push(m[i].Index);
+      }
+      else
+      {
+        int i1 = Q.Pop();
+        int nb = m[i].Index - i1;
+        string temp = l.Substring(i1 + 1, nb - 1);
+        long ans = compute(temp, rule);
+        l = l.Remove(i1, nb + 1).Insert(i1, ans.ToString().PadRight(nb + 1));
+        // Console.WriteLine($"  temp : {temp}");
+        // Console.WriteLine($"l : {l}");
+
+      }
+      i++;
+    }
+  }
+  return compute(l, rule);
 }
 
 void part1()
 {
   long ans = 0;
-  file.ForEach(l => {
-    ans += resolve(l);
+  file.ForEach(l =>
+  {
+    // Console.ForegroundColor = ConsoleColor.DarkRed;
+    // Console.WriteLine($"T : {l}");
+    // Console.ForegroundColor = ConsoleColor.White;
+    long r = resolve(l, ['*', '+']);
+    // Console.ForegroundColor = ConsoleColor.Green;
+    // Console.WriteLine($"Total : {r}");
+    // Console.ForegroundColor = ConsoleColor.White;
+    ans += r;
   });
   Console.WriteLine($"Part 1 - Answer : {ans}");
 }
@@ -31,8 +111,18 @@ void part1()
 
 void part2()
 {
-  int ans = 0;
-
+  long ans = 0;
+  file.ForEach(l =>
+  {
+    // Console.ForegroundColor = ConsoleColor.DarkRed;
+    // Console.WriteLine($"T : {l}");
+    // Console.ForegroundColor = ConsoleColor.White;
+    long r = resolve(l, ['+', '*']);
+    // Console.ForegroundColor = ConsoleColor.Green;
+    // Console.WriteLine($"Total : {r}");
+    // Console.ForegroundColor = ConsoleColor.White;
+    ans += r;
+  });
   Console.WriteLine($"Part 2 - Answer : {ans}");
 }
 
--- 18/obj/Debug/net9.0/18.AssemblyInfo.cs
+++ 18/obj/Debug/net9.0/18.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("18")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c18f1904a8e8d3e0a1c7f2fea2cd2062d443d17e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db624d989eacb3c37ac82446e53d9fa2455c66a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("18")]
 [assembly: System.Reflection.AssemblyTitleAttribute("18")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
