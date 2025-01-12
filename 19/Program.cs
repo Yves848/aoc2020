f8466d268d4d7f48f66abc66d650df81e7a1b31b
yves848 <yves.godart@gmail.com>
2025-01-12 16:13:04 +0100

    19

---
+++ 19/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+var file = args.Length > 0 ? File.ReadAllText(args[0]) : File.ReadAllText($"{home}/git/aoc2020/19/test.txt");
+var blocs = file.Split("\n\n");
+
+void part1()
+{
+  int ans = 0;
+  Console.WriteLine(blocs);
+  Console.WriteLine($"Part 1 - Answer : {ans}");
+}
+
+void print(string str, bool valid)
+{
+  if (valid)
+  {
+    Console.ForegroundColor = ConsoleColor.Green;
+  }
+  else
+  {
+    Console.ForegroundColor = ConsoleColor.Red;
+  }
+  Console.Write($"{str} ");
+}
+
+void part2()
+{
+  int ans = 0;
+
+  Console.WriteLine($"Part 2 - Answer : {ans}");
+}
+
+part1();
+
+part2();
