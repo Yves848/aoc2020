440e7ca2adbaf8fff725b79a6797082f043bfb8d
yves848 <yves.godart@gmail.com>
2025-01-18 17:50:31 +0100

    wip

--- 20/Program.cs
+++ 20/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System.Text.RegularExpressions;
 using System.Runtime.InteropServices;
+using System.Security.Authentication;
+using System.Runtime.Serialization;
+using System.Data.Common;
 
 string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
 var file = args.Length > 0 ? File.ReadAllText(args[0]) : File.ReadAllText($"{home}/git/aoc2020/20/test.txt");
@@ -98,26 +101,92 @@ string[,] RotateClockwise(string[,] matrix)
   return result;
 }
 
-void PrintMatrix(string[,] matrix)
-    {
-        int rows = matrix.GetLength(0);
-        int cols = matrix.GetLength(1);
+string[,] transform(string[,] matrix, int t)
+{
+  int rows = matrix.GetLength(0);
+  int cols = matrix.GetLength(1);
+  string[,] result = new string[cols, rows];
+  switch (t)
+  {
+    case 1:
+      result = FlipVertically(matrix);
+      break;
+    case 2:
+      result = FlipHorizontally(matrix);
+      break;
+    case 3:
+      result = RotateClockwise(matrix);
+      break;
+    case 4:
+      result = RotateClockwise(matrix); result = RotateClockwise(result);
+      break;
+    case 5:
+      result = RotateClockwise(matrix); result = RotateClockwise(result); result = RotateClockwise(result);
+      break;
 
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-                Console.Write(matrix[i, j]);
-            Console.WriteLine();
-        }
-    }
+  }
+  return result;
+}
+
+void PrintMatrix(string[,] matrix)
+{
+  int rows = matrix.GetLength(0);
+  int cols = matrix.GetLength(1);
+
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
+      Console.Write(matrix[i, j]);
+    Console.WriteLine();
+  }
+}
 
 void part1()
 {
   int ans = 0;
-  var grid = tiles.ToList();
-  PrintMatrix(grid[0].Value.tile);
-  Console.WriteLine($"");
-  PrintMatrix(FlipVertically(grid[0].Value.tile));
+  var list = tiles.ToList();
+  int side = (int)Math.Sqrt((double)list.Count);
+  Dictionary<int, ((int, int), List<string>)> grid = [];
+  int i = 0;
+  // recherche des coins
+  while (i < list.Count)
+  {
+    var (num, piece) = list[i];
+    // PrintMatrix(piece.tile);
+    var g = piece.tile;
+    var b = borders(g);
+    int t = 0;
+    while (t < 6 && !grid.ContainsKey(num))
+    {
+    (int U, int L, int R, int D) cont = (U: 0, L: 0, R: 0, D: 0);
+      int j = 0;
+      while (j < list.Count)
+      {
+        if (j != i)
+        {
+          var (num2, piece2) = list[j];
+          var g2 = piece2.tile;
+          int t2 = 0;
+          while (t2 < 6)
+          {
+            var b2 = borders(g2);
+            if (b.L == b2.R) cont.L++;
+            if (b.U == b2.D) cont.U++;
+            if (b.D == b2.U) cont.D++;
+            if (b.R == b2.L) cont.R++;
+            t2++;
+            g2 = transform(piece2.tile,t2);
+          }
+        }
+        j++;
+      }
+      t++;
+      g = transform(piece.tile,t);
+      b = borders(g);
+    }
+    i++;
+  }
+
   Console.WriteLine($"Part 1 - Answer : {ans}");
 }
 
